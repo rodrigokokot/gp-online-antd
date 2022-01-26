5b4b85e62076e3aadf24f9118f750705d9021bd3
Rodrigo Kokot <rodrigokokot@gmail.com>
2022-01-26 14:13:09 -0300

    changes

--- src/components/molecules/Breadcrum/index.jsx
+++ src/components/molecules/Breadcrum/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Breadcrumb } from 'antd';
+import { useLocation } from 'react-router-dom';
+
 
 const BreadcrumComponent = ({props}) => {
+  const location = useLocation();
+  const arrayLocation = location.pathname.split('/')
+
   return (
-    <Breadcrumb separator=">">
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
+    arrayLocation.length > 2 &&  <Breadcrumb separator=">">
+        {/* <Breadcrumb.Item>Home</Breadcrumb.Item>
         <Breadcrumb.Item href="">Application Center</Breadcrumb.Item>
         <Breadcrumb.Item href="">Application List</Breadcrumb.Item>
-        <Breadcrumb.Item>An Application</Breadcrumb.Item>
+        <Breadcrumb.Item>An Application</Breadcrumb.Item> */}
+      {
+        arrayLocation.map(( element, index ) =>
+          element.length > 0 &&  <Breadcrumb.Item href="" key={index}>{ (element.charAt(0).toUpperCase() + element.slice(1) ).split(/(?=[A-Z])/).join(' ')}</Breadcrumb.Item>
+        )
+      }
     </Breadcrumb>
   )
 };
