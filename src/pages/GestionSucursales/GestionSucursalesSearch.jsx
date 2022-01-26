769fe9d0c1c51f8676e4345deec3de50c80daf94
Deimos999 <facundo42081757@gmail.com>
2022-01-26 10:50:06 -0300

    26/01

--- src/pages/GestionSucursales/GestionSucursalesSearch.jsx
+++ src/pages/GestionSucursales/GestionSucursalesSearch.jsx
@@ -2,67 +2,16 @@ import Table from "../../components/organisms/Table/index";
 import React, { useState } from "react";
 import SearchForm from "../../components/organisms/SearchForm/index";
 import { GestionSucursalesSearchMock } from "../../Mocks/GestionSucursalesSearchMock";
-import { Card, Button, Col, Switch, Form, Row, Modal} from "antd";
+import { Card, Button, Col} from "antd";
 import {
   columnsGestionSucursales,
   dataGestionSucursales,
 } from "../../Mocks/GestionSucursales";
-import { useHistory, Link} from "react-router-dom";
-import FloatInput from '../../components/molecules/FloatInput/index'
-import FloatSelect from '../../components/molecules/FloatSelected/index'
+import {Link} from "react-router-dom";
 
 
 function GestionSucursales() {
   const [data, setData] = useState("");
-  const history = useHistory();
-  const [flag, setFlag] = useState(false); //Flag de página para mostrar busqueda o editar/crear
-  const [editflag, setEditflag] = useState(false); //Flag de edición para determinar formulario de creación o edición
-  const [form] = Form.useForm();
-
-  const [isModalVisible, setIsModalVisible] = useState(false); 
-
-  
-  const setModalVisible = () => {
-    setIsModalVisible(true);
-  };
-
-  const onFinish = (values) =>{
-    console.log(values)
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-
-  const setCrearSucursal = () => {
-    setFlag(true);
-    setEditflag(false);
-  }
-
-  const setEditarSucursal = () =>{
-    setFlag(true);
-    setEditflag(true);
-  }
-
-  const setBusquedaSucursal = () =>{
-    setFlag(false);
-    setEditflag(false);
-  }
-
-  
-
-
-
-   const routeChange = () => {
-     let path = "gestionSucursales/editarSucursal/new";
-
-     history.push(path);
-   };
-
 
   const handleCallback = (values) => {
     if (
