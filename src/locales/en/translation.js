ab5fe0b2edf8b03edb607d94186cc1d8582e90f3
Rodrigo Kokot <rodrigokokot@gmail.com>
2022-02-07 11:19:34 -0300

    traslate

--- src/locales/en/translation.js
+++ src/locales/en/translation.js
@@ -1,4 +1,20 @@
 export const INGLES = {
+  "login": {
+    "username": {
+      "label": "user",
+      "error": "You must enter a user"
+    },
+    "password": {
+      "label": "password",
+      "error": "You must enter a password"
+    },
+    "rememberme": "Remember me",
+    "btnLogin": "Login",
+    "errorLogin": {
+      "title": "Incorrect data entered, please try again.",
+      "description": "If you forgot your password, please contact our computer security team to help you."
+    }
+  },
     "title": "Welcome to react using react-i18next",
     "description": {
       "part1": "To get started, edit <1>src/App.js</1> and save to reload.",
