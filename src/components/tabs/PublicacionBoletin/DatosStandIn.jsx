961d2b202023e86041e980f5f88b415a981b38e5
Emiliano <emiliano.eze.12@gmail.com>
2022-02-07 23:33:24 -0300

    datastandin check

--- src/components/tabs/PublicacionBoletin/DatosStandIn.jsx
+++ src/components/tabs/PublicacionBoletin/DatosStandIn.jsx
@@ -5,17 +5,16 @@ const { Text } = Typography;
 
 const DatosStandIn= () => {
     
-    const [checkedList1, setCheckedList1]=useState('');
+    const [checkedList1, setCheckedList1]=useState("");
     function onChangeCheck(list){
-        setCheckedList1(list); 
-      }; 
-    
+        setCheckedList1(list.target.value);
+      };  
   return (<>
     <Card>
         <h4>Fecha Vigencia S.I.</h4><Title level={4}>{'31/12/2017'}</Title>
         <br></br><h4>Datos de EWBF</h4>
         <Row gutter={48}>
-        <Col span={3}>
+        <Col span={4}>
         <Title level={5}>{'Región 0: '}</Title>
         <Title level={5}>{'Región 1: '}</Title>
         <Title level={5}>{'Región A: '}</Title>
@@ -24,29 +23,27 @@ const DatosStandIn= () => {
         <Title level={5}>{'Región D: '}</Title>
         <Title level={5}>{'Región E: '}</Title> 
         </Col>
-        <Col span={4} ><Form.Item name='regiones'>  
-                <Checkbox.Group  onChange={onChangeCheck}>
-                    <Checkbox value='Región 0'></Checkbox> <br/><br/>
-                    <Checkbox  value='Región 1'></Checkbox> <br/><br/>
-                    <Checkbox  value='Región A'></Checkbox><br/><br/>
-                    <Checkbox  value='Región B'></Checkbox>   <br/><br/>
-                    <Checkbox  value='Región C'></Checkbox> <br/><br/>
-                    <Checkbox  value='Región D'></Checkbox> <br/><br/>
-                    <Checkbox  value='Región E'></Checkbox><br/><br/>
-                     
-                </Checkbox.Group>
-           </Form.Item> 
+        <Col span={5} ><Form.Item name='regiones'>   
+                    <Checkbox value='Region 0' onChange={onChangeCheck}></Checkbox> <br/><br/>
+                    <Checkbox  value='Region 1' onChange={onChangeCheck}></Checkbox> <br/><br/>
+                    <Checkbox  value='Region A' onChange={onChangeCheck}></Checkbox><br/><br/>
+                    <Checkbox  value='Region B' onChange={onChangeCheck}></Checkbox>   <br/><br/>
+                    <Checkbox  value='Region C' onChange={onChangeCheck}></Checkbox> <br/><br/>
+                    <Checkbox  value='Region D' onChange={onChangeCheck}></Checkbox> <br/><br/>
+                    <Checkbox  value='Region E' onChange={onChangeCheck}></Checkbox><br/><br/>  
+       
+           </Form.Item>  
+        </Col>
+        <Col span={5}>
+        <Title level={5}>{'Fecha Vigencia'}</Title>
+        <Title level={5}>{'Fecha Vigencia'}</Title>
+        <Title level={5}>{'Fecha Vigencia'}</Title>
+        <Title level={5}>{'Fecha Vigencia'}</Title>
+        <Title level={5}>{'Fecha Vigencia'}</Title>
+        <Title level={5}>{'Fecha Vigencia'}</Title>
+        <Title level={5}>{'Fecha Vigencia'}</Title>
         </Col>
         <Col span={4}>
-        <Title level={5}>{'Fecha Vigencia'}</Title>
-        <Title level={5}>{'Fecha Vigencia'}</Title>
-        <Title level={5}>{'Fecha Vigencia'}</Title>
-        <Title level={5}>{'Fecha Vigencia'}</Title>
-        <Title level={5}>{'Fecha Vigencia'}</Title>
-        <Title level={5}>{'Fecha Vigencia'}</Title>
-        <Title level={5}>{'Fecha Vigencia'}</Title>
-        </Col>
-        <Col span={3}>
         <Text underline>{'31/12/2017'}</Text><br/><br/>
         <Text underline>{'31/12/2017'}</Text><br/><br/>
         <Text underline>{'31/12/2017'}</Text><br/><br/>
@@ -57,12 +54,7 @@ const DatosStandIn= () => {
         </Col>
         </Row>
     </Card> <br></br>
-  </>      
-            
-            
-            
-            
-             
+  </>        
   );
 };
 export default DatosStandIn;
